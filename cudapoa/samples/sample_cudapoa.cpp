9857bd41aa3e8af2bf78378bff683b68935bd3d3
00James00 <69451255+00James00@users.noreply.github.com>
2023-11-18 18:31:44 -0500

    Update sample_cudapoa.cpp

--- cudapoa/samples/sample_cudapoa.cpp
+++ cudapoa/samples/sample_cudapoa.cpp
@@ -200,7 +200,7 @@ int main(int argc, char** argv)
 
 
     std::vector<std::vector<std::string>> windows;
-    std::vector<std::string> input_paths = {"/home/bc526/GenomeWorks/build/cudapoa/samples/cuda/cudapoa_supplement/DRB1-3123.fa
+    std::vector<std::string> input_paths = {"/home/bc526/GenomeWorks/build/cudapoa/samples/DRB1-3123.fa
 "};
     parse_fasta_files(windows, input_paths, -1); // -1 to read all windows
 
